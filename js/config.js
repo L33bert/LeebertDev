268d2a6b84dfdb89dfb9847cb5f4c099e8df1c8f
EC2 Default User <ec2-user@ip-172-31-95-223.ec2.internal>
2020-01-10 17:11:11 +0000

    conf

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_ayV2QiLLq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1vn9blj5bib8s6g67vsqd8p3g3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'b48knrh7ooqhg7475v5olsvde', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
